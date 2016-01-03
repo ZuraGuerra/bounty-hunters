a8b026c71a9e1bff42abbae5053a25c276558501
Zura Guerra <zuraweep@gmail.com>
2016-01-03 12:22:13 -0600

    Generate cool random avatars

---
+++ lib/alphabet.ex
@@ -0,0 +1,34 @@
+defmodule Elbuencoffi.Alphabet do
+	@alphabet %{
+		"a" => 1,
+	    "b" => 2,
+	    "c" => 3,
+	    "d" => 4,
+	    "e" => 5,
+	    "f" => 6,
+	    "g" => 7,
+	    "h" => 8,
+	    "i" => 9,
+	    "j" => 10,
+	    "k" => 1,
+	    "l" => 2,
+	    "m" => 3,
+	    "n" => 4,
+	    "o" => 5,
+	    "p" => 6,
+	    "q" => 7,
+	    "r" => 8,
+	    "s" => 9,
+	    "t" => 10,
+	    "u" => 1,
+	    "v" => 2,
+	    "w" => 3,
+	    "x" => 4,
+	    "y" => 5,
+	    "z" => 6
+	}
+
+	 def alphabet do
+	 	@alphabet
+	 end
+end
--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,7 @@ defmodule Elbuencoffi.PageController do
   use Elbuencoffi.Web, :controller
 
   def index(conn, _params) do
+  	Elbuencoffi.RandomAvatar.generate("bounty")
     render conn, "index.html"
   end
 end
